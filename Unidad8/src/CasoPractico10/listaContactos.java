8c6f5d6030370e6cbe6314f05e7f79d9b9f1a1b3
ignaciocastillon_ <ignacio.s416714@cesurformacion.com>
2025-04-08 21:03:33 +0200

    Unidad 8

--- Unidad8/src/CasoPractico10/listaContactos.java
+++ Unidad8/src/CasoPractico10/listaContactos.java
@@ -2,6 +2,7 @@ package CasoPractico10;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
 public class listaContactos extends JFrame {
@@ -21,6 +22,7 @@ public class listaContactos extends JFrame {
     private boolean encontrado = false;
     private int contador = 0;
     private int agregados = 0;
+    private boolean cargado = false;
 
 
     public listaContactos() {
@@ -38,17 +40,24 @@ public class listaContactos extends JFrame {
         guardar.addActionListener(e -> {
             acciones(e);
         });
+        categoria.addActionListener(e-> {
+            acciones(e);
+        });
+        JOptionPane.showMessageDialog(panel1, "Para cargar los contantos pulse el boton 'Cargar contactos'");
     }
 
     private void acciones(ActionEvent e) {
         if (e.getSource() == cargar) {
-        cargarContactos();
+            cargarContactos();
         }
         if (e.getSource() == nuevo) {
-        nuevoContacto();
+            nuevoContacto();
         }
         if (e.getSource() == guardar) {
-        guardarContactos();
+            guardarContactos();
+        }
+        if (e.getSource() == categoria){
+            buscador();
         }
     }
 
@@ -79,59 +88,84 @@ public class listaContactos extends JFrame {
     }
 
     private void nuevoContacto() {
-            if (nombre.getText().isEmpty() || telefono.getText().isEmpty() || email.getText().isEmpty() || categoria.getSelectedItem().toString().isEmpty()) {
-                JOptionPane.showMessageDialog(panel1, "Debe ingresar los campos");
+        if (nombre.getText().isEmpty() || telefono.getText().isEmpty() || email.getText().isEmpty() || categoria.getSelectedItem().toString().isEmpty()) {
+            JOptionPane.showMessageDialog(panel1, "Debe ingresar los campos");
+        } else {
+            contactos[contador] = new Contacto(nombre.getText(),telefono.getText(),email.getText(),categoria.getSelectedItem().toString());
+            contador++;
+            if (agregados == 0) {
+                area.setText("Nombre: " + nombre.getText() + "\nTelefono: " + telefono.getText() + "\nE-mail: " + email.getText() + "\nCategoria: " + categoria.getSelectedItem().toString());
+                agregados++;
             } else {
-                if (agregados == 0) {
-                    area.setText("Nombre: " + nombre.getText() + "\nTelefono: " + telefono.getText() + "\nE-mail: " + email.getText() + "\nCategoria: " + categoria.getSelectedItem().toString());
-                    agregados++;
-                } else {
-                    area.setText(area.getText() + "\n\nNombre: " + nombre.getText() + "\nTelefono: " + telefono.getText() + "\nE-mail: " + email.getText() + "\nCategoria: " + categoria.getSelectedItem().toString());
-                    agregados++;
-                }
+                area.setText(area.getText() + "\n\nNombre: " + nombre.getText() + "\nTelefono: " + telefono.getText() + "\nE-mail: " + email.getText() + "\nCategoria: " + categoria.getSelectedItem().toString());
+                agregados++;
             }
+            vaciar();
+        }
     }
 
     private void guardarContactos() {
         if (!archivo.exists()) {
             try {
                 archivo.createNewFile();
-                JOptionPane.showMessageDialog(panel1,"Archivo creado");
+                JOptionPane.showMessageDialog(panel1, "Archivo creado");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
-        String[] lineas = area.getText().split("\n\n");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true))) {
-            for (int i = 0; i < agregados; i++) {
-                    writer.write(lineas[i] + "\n\n");
-
+        if (cargado) {
+            int confirmado = JOptionPane.showConfirmDialog(panel1, "Estas seguro de que quieres añadir los contactos del area, se sustituira todo el archivo por lo escrito en el area");
+            if (JOptionPane.OK_OPTION == confirmado) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, false))) {
+                    String[] lineas = area.getText().split("\n\n");
+                    for (int i = 0; i < agregados; i++) {
+                        writer.write(lineas[i] + "\n\n");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                JOptionPane.showMessageDialog(panel1, "Proceso cancelado");
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } else {
+            int confirmado = JOptionPane.showConfirmDialog(panel1, "Estas seguro de que quieres añadir los contactos del area a los contactos existentes?");
+            if (JOptionPane.OK_OPTION == confirmado) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true))) {
+                    String[] lineas = area.getText().split("\n\n");
+                    for (int i = 0; i < agregados; i++) {
+                        writer.write(lineas[i] + "\n\n");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                JOptionPane.showMessageDialog(panel1, "Proceso cancelado");
+            }
         }
     }
 
-        private void cargarContactos(){
-        JFileChooser j = new JFileChooser();
+    private void cargarContactos() {
+        File rutaproyecto = new File(System.getProperty("user.dir"));
+        JFileChooser j = new JFileChooser(rutaproyecto);
         j.setDialogTitle("Selecciona un archivo .csv");
         int resultado = j.showOpenDialog(panel1);
 
-        if (resultado==JFileChooser.APPROVE_OPTION){
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            cargado = true;
             File archivo = j.getSelectedFile();
-            try(BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
                 String linea;
                 contador = 0;
                 int contador2 = 0;
-                while ((linea=reader.readLine())!=null){
-                    if (!linea.isEmpty()){
+                while ((linea = reader.readLine()) != null) {
+                    if (!linea.isEmpty()) {
                         String[] nombre = linea.split(":");
                         String[] telefono = reader.readLine().split(":");
                         String[] email = reader.readLine().split(":");
                         String[] categoria = reader.readLine().split(":");
-                        Contacto contacto = new Contacto(nombre[1],telefono[1],email[1],categoria[1]);
-                            contactos[contador] = contacto;
-                            contador++;
+                        Contacto contacto = new Contacto(nombre[1], telefono[1], email[1], categoria[1]);
+                        contactos[contador] = contacto;
+                        contador++;
 
                     }
                 }
@@ -142,18 +176,87 @@ public class listaContactos extends JFrame {
                 throw new RuntimeException(e);
             }
         }
-        }
-        private void mostrarContactos(){
+    }
+
+    private void mostrarContactos() {
         agregados = 0;
         area.setText("");
-        for (int i=0;i<contador;i++){
-            if (contactos[i]!=null){
-                if (agregados==0) {
+        for (int i = 0; i < contador; i++) {
+            if (contactos[i] != null) {
+                if (agregados == 0) {
                     area.setText(contactos[i].toString());
                     agregados++;
-                }else{
-                    area.setText(area.getText()+"\n\n"+contactos[i].toString());
+                } else {
+                    area.setText(area.getText() + "\n\n" + contactos[i].toString());
                     agregados++;
+                }
+            }
+        }
+    }
+
+    private void vaciar() {
+        nombre.setText("");
+        telefono.setText("");
+        email.setText("");
+    }
+
+    private void buscador() {
+        if (categoria.getSelectedItem().equals("Familia")) {
+            area.setText("");
+            for (int i = 0; i < contador; i++) {
+                String[] datos = contactos[i].toString().split("\n");
+                String[] datos2 = datos[3].split(": ");
+                if (contactos[i] != null && categoria.getSelectedItem().equals(datos2[1].trim())) {
+                    if (agregados == 0) {
+                        area.setText(contactos[i].toString());
+                        agregados++;
+                    } else {
+                        area.setText(area.getText() + "\n\n" + contactos[i].toString());
+                        agregados++;
+                    }
+                }
+            }
+        }else if (categoria.getSelectedItem().equals("Amigos")) {
+            area.setText("");
+            for (int i = 0; i < contador; i++) {
+                String[] datos = contactos[i].toString().split("\n");
+                String[] datos2 = datos[3].split(": ");
+                if (contactos[i] != null && categoria.getSelectedItem().equals(datos2[1].trim())) {
+                    if (agregados == 0) {
+                        area.setText(contactos[i].toString());
+                        agregados++;
+                    } else {
+                        area.setText(area.getText() + "\n\n" + contactos[i].toString());
+                        agregados++;
+                    }
+                }
+            }
+        }else if (categoria.getSelectedItem().equals("Trabajo")) {
+            area.setText("");
+            for (int i = 0; i < contador; i++) {
+                String[] datos = contactos[i].toString().split("\n");
+                String[] datos2 = datos[3].split(": ");
+                if (contactos[i] != null && categoria.getSelectedItem().equals(datos2[1].trim())) {
+                    if (agregados == 0) {
+                        area.setText(contactos[i].toString());
+                        agregados++;
+                    } else {
+                        area.setText(area.getText() + "\n\n" + contactos[i].toString());
+                        agregados++;
+                    }
+                }
+            }
+        }else if (categoria.getSelectedItem().equals("Todas")) {
+            area.setText("");
+            for (int i = 0; i < contador; i++) {
+                if (contactos[i] != null) {
+                    if (agregados == 0) {
+                        area.setText(contactos[i].toString());
+                        agregados++;
+                    } else {
+                        area.setText(area.getText() + "\n\n" + contactos[i].toString());
+                        agregados++;
+                    }
                 }
             }
         }
